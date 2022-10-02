9b42b50751c5f6b3f37f90981ee263750fedcd08
Aeby Nathalie <nathalie.aeby@students.bfh.ch>
2022-10-02 16:01:28 +0200

    Added FastAPI

--- test/test_fizz_buzz.py
+++ test/test_fizz_buzz.py
@@ -5,8 +5,8 @@ import fizzbuzz.fizzbuzz as fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_this_will_break(self):
-        self.assertEqual(fizzbuzz.fizzbuzz(self,1),"3")
+    #def test_this_will_break(self):
+      #  self.assertEqual(fizzbuzz.fizzbuzz(self,1),"3")
 
     def test_one_returns_one_as_string(self):
         self.assertEqual(type(fizzbuzz.fizzbuzz(self,1)), str)
