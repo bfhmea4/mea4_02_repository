3b22da58cc486d2379acc73ba3e2ad9ef406599c
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2022-10-06 10:25:33 +0200

    database first

---
+++ migrations/0001.create-foo.sql
@@ -0,0 +1,4 @@
+--
+-- file: migrations/0001.create-foo.sql
+--
+CREATE TABLE foo (id INT, bar VARCHAR(20), PRIMARY KEY (id));
---
+++ migrations/0001_create_foo.py
@@ -0,0 +1,20 @@
+#
+# file: migrations/0001_create_foo.py
+#
+from yoyo import step
+
+def apply_step(conn):
+    cursor = conn.cursor()
+    cursor.execute(
+        # query to perform the migration
+    )
+
+def rollback_step(conn):
+    cursor = conn.cursor()
+    cursor.execute(
+        # query to undo the above
+    )
+
+steps = [
+    step(apply_step, rollback_step)
+]
