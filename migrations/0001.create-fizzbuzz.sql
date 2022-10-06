a88e4d493d0a8091bbbc75e49231fcc9c3fdd779
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2022-10-06 10:53:04 +0200

    database second

---
+++ migrations/0001.create-fizzbuzz.sql
@@ -0,0 +1,4 @@
+--
+-- file: migrations/0001.create-fizzbuzz.sql
+--
+CREATE TABLE fizzbuzz (id INT, response VARCHAR(20), request INT, PRIMARY KEY (id));
--- migrations/0001.create-foo.sql
+++
@@ -1,4 +0,0 @@
---
--- file: migrations/0001.create-foo.sql
---
-CREATE TABLE foo (id INT, bar VARCHAR(20), PRIMARY KEY (id));
