9c7e2799e852b5a86435bc19420414b533d7c746
Aeby Nathalie <nathalie.aeby@students.bfh.ch>
2022-10-06 16:03:07 +0200

    added logging

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@ from typing import Union
 import fastapi
 import fizzbuzz.fizzbuzz as fizzbuzz
 import uvicorn
+import logging
 
+
+logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 app = fastapi.FastAPI()
 
-#Kann man auch auf Command Line machen
+
 if __name__ == "__main__":
+    logging.info("test")
     uvicorn.run("main:app", port=8000, log_level="debug")
 
 @app.get("/{number}")
