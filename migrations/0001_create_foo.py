86c9a70bcc9ecefd565a93846091885caeccbc2a
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2022-10-06 15:16:21 +0200

    docker-compose.yml file for sqlite3

--- migrations/0001_create_foo.py
+++ migrations/0001_create_foo.py
@@ -3,17 +3,20 @@
 #
 from yoyo import step
 
+
 def apply_step(conn):
     cursor = conn.cursor()
     cursor.execute(
         # query to perform the migration
     )
 
+
 def rollback_step(conn):
     cursor = conn.cursor()
     cursor.execute(
         # query to undo the above
     )
+
 
 steps = [
     step(apply_step, rollback_step)
