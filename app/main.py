34ec2ed9c9e93c0df6a36ffebec749338ca41972
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2022-10-12 18:42:03 +0200

    change docker-compose.yml

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 import venv
 
-import data as data
+import sqlite3
 import fastapi
 import uvicorn
 import fizzbuzz.fizzbuzz as fizzbuzz
 
 app = fastapi.FastAPI()
 
+
+def apply_step():
+    conn = sqlite3.connect('/C:/Users/bernh/git/pycharm/mea4_02_repository/data/db/db.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE fizzbuzz
+    (id INTEGER PRIMARY KEY ASC, request int, response varchar(50))
+''')
+    conn.commit()
+    conn.close()
+
+
 # Kann man auch auf Command Line machen
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, log_level="debug")
 
-venv.data.post.apply_step()
+apply_step()
 
 
 @app.get("/{number}")
