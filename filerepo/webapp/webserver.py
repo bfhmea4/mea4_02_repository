0acca1bb7d3b975f3c0f3a5922d174f603b88ea4
Hu6li <jens@w00k55.pnet.ch>
2022-10-13 16:00:33 +0200

    Issue #17 Delete file in repo based on name Implemented basic tests, test should run after upload tests

--- filerepo/webapp/webserver.py
+++ filerepo/webapp/webserver.py
@@ -1,9 +1,11 @@
 from typing import Union
-import fastapi
+from fastapi import FastAPI, File, UploadFile, status
+from fastapi.responses import JSONResponse
 import filerepo.algorithms.fizzbuzz as fizzbuzz
 import uvicorn
+from pathlib import Path
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
 def run():
     uvicorn.run("filerepo.webapp.webserver:app", port=8000, log_level="debug")
@@ -11,6 +13,27 @@ def run():
 @app.get("/{number}")
 def read_item(number: int):
     return {"output": fizzbuzz.fizzbuzz(number)}
+
+@app.delete("/files/{filename}", status_code=204)
+def delete_file(filename: str):
+    try:
+        Path("/opt/repository/"+filename).unlink()
+    except FileNotFoundError as e:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={'message': str(e)}
+            )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={'message': str(e)}
+            )
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"result": 'success'}
+        )
+
 
 #Kann man auch auf Command Line machen
 if __name__ == "__main__":
