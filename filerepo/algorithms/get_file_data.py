4771b3be902d4d4e5a7c7486eb3bbce633b46647
Aeby Nathalie <nathalie.aeby@students.bfh.ch>
2022-10-17 13:17:46 +0200

    added file data informations

--- filerepo/algorithms/get_file_data.py
+++ filerepo/algorithms/get_file_data.py
@@ -1,17 +1,19 @@
 import json
 import os
+from datetime import datetime
 
 
 def get_file_informations(file: str) -> str:
     size = os.path.getsize(file)
-    lastModify= os.path.getmtime(file)
-    creationDate = os.path.getctime(file)
-    fileType = os.path.splitext(file)
+    last_modify= datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+    print(last_modify)
+    creation_date = datetime.fromtimestamp(os.path.getctime(file)).strftime('%Y-%m-%d %H:%M:%S')
+    file_type = os.path.splitext(file)
 
-    data = {"size: ": size,
-            "last Modify: ": lastModify,
-            "creation Date: ": creationDate,
-            "file typ: ": fileType}
+    data = {"size in bytes: ": size,
+            "last Modify: ": last_modify,
+            "creation Date: ": creation_date,
+            "file typ: ": file_type[1]}
     json_file = json.loads(json.dumps(data))
     return json_file
 
--- filerepo/webapp/webserver.py
+++ filerepo/webapp/webserver.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, File, UploadFile, status
 from fastapi.responses import JSONResponse, FileResponse
 import aiofiles
 import filerepo.algorithms.fizzbuzz as fizzbuzz
+import filerepo.algorithms.get_file_data as getFileData
 import uvicorn
 from pathlib import Path
 import json
@@ -12,17 +13,20 @@ app = FastAPI()
 
 Path("/opt/repository").mkdir(parents=True, exist_ok=True)
 
+
 def run():
     uvicorn.run("filerepo.webapp.webserver:app", port=8000, log_level="debug")
+
 
 @app.get("/fizzbuzz/{number}")
 def read_item(number: int):
     return {"output": fizzbuzz.fizzbuzz(number)}
 
+
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
     try:
-        async with aiofiles.open("/opt/repository/"+file.filename, 'wb') as out_file:
+        async with aiofiles.open("/opt/repository/" + file.filename, 'wb') as out_file:
             content = await file.read()  # async read
             await out_file.write(content)  # async write
 
@@ -30,22 +34,23 @@ async def upload(file: UploadFile = File(...)):
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={'message': str(e)}
-            )
+        )
     except Exception as e:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={'message': str(e)}
-            )
+        )
     else:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"result": 'success'}
         )
 
+
 @app.get("/files")
 def files():
     try:
-        files_json=json.dumps(os.listdir("C:/Users/naeby/Documents/BFH/Project1"))
+        files_json = json.dumps(os.listdir("C:/Users/naeby/Documents/BFH/Project1"))
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"files": files_json}
@@ -55,81 +60,88 @@ def files():
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={'message': str(e)}
-            )
+        )
     except Exception as e:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={'message': str(e)}
-            )
+        )
+
 
 @app.delete("/files/{filename}", status_code=204)
 def delete_file(filename: str):
     try:
-        Path("/opt/repository/"+filename).unlink()
+        Path("/opt/repository/" + filename).unlink()
     except FileNotFoundError as e:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={'message': str(e)}
-            )
+        )
     except Exception as e:
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={'message': str(e)}
-            )
+        )
     else:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"result": 'success'}
         )
 
+
 @app.get("/files/{filename}", status_code=200)
 async def download_file(filename: str):
     try:
-        if Path("C:/Users/naeby/Documents/BFH/Project1/"+filename).is_file():
-            return FileResponse("C:/Users/naeby/Documents/BFH/Project1/"+filename, media_type='application/octet-stream',filename=filename)
+        if Path("C:/Users/naeby/Documents/BFH/Project1/" + filename).is_file():
+            return FileResponse("C:/Users/naeby/Documents/BFH/Project1/" + filename,
+                                media_type='application/octet-stream', filename=filename)
         else:
             raise FileNotFoundError
     except FileNotFoundError as e:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={'message': "FileNotFound"}
-            )
+        )
     except Exception as e:
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={'message': str(e)}
-            )
+        )
     else:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"result": 'success'}
         )
 
-    @app.get("/files/{filename}/info", status_code=200)
-    async def get_file_info(filename: str):
-        try:
-            if Path("C:/Users/naeby/Documents/BFH/Project1/" + filename).is_file():
 
-                return FileResponse("C:/Users/naeby/Documents/BFH/Project1/" + filename,
-                                    media_type='application/octet-stream', filename=filename)
-            else:
-                raise FileNotFoundError
-        except FileNotFoundError as e:
-            return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND,
-                content={'message': "FileNotFound"}
-            )
-        except Exception as e:
-            return JSONResponse(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content={'message': str(e)}
-            )
-        else:
+@app.get("/files/{filename}/info", status_code=200)
+async def get_file_info(filename: str):
+    try:
+        if Path("C:/Users/naeby/Documents/BFH/Project1/" + filename).is_file():
+            file_json = getFileData.get_file_informations("C:/Users/naeby/Documents/BFH/Project1/" + filename)
             return JSONResponse(
                 status_code=status.HTTP_200_OK,
-                content={"result": 'success'}
+                content={"file Infos": file_json}
             )
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError as e:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={'message': "FileNotFound"}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={'message': str(e)}
+        )
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"files": file_json}
+        )
 
-#Kann man auch auf Command Line machen
+
+# Kann man auch auf Command Line machen
 if __name__ == "__main__":
     run()
